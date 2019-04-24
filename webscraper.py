f9e6f236c9d0157aae295326b41a9ef44e3cd95c
wathiqabumaali <aeaabumaali@ucdavis.edu>
2019-04-23 21:05:53 -0700

    Update
    Getting somewhere

--- webscraper.py
+++ webscraper.py
@@ -1,9 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 url = "http://www.espn.com/nba/playbyplay?gameId=401126819"
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
 
-for play in content.findAll('tr'):
-    print play.text.encode('utf-8')
+playArray = []
+for play in content.findAll('div', attrs={"id": "gp-quarter-1"}):
+    playObject = {
+        "timestamp": play.find('td', attrs={"class": "time-stamp"}).text.encode('utf-8'),
+        "gameDetails": play.find('td', attrs={"class": "game-details"}).text.encode('utf-8'),
+        "combinedScore": play.find('td', attrs={"class": "combined-score"}).text.encode('utf-8'),
+    }
+    print playObject
+    
