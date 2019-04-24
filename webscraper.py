1558eb6038ed63c4e34bd1db976efe8c6c0864ce
wathiqabumaali <aeaabumaali@ucdavis.edu>
2019-04-23 21:22:22 -0700

    Update
    Trying to get code to loop through all 4 quarters of the game. I'm only getting 3 though. Something isn't right

--- webscraper.py
+++ webscraper.py
@@ -7,13 +7,16 @@ response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
 
 playArray = []
-for play in content.findAll('div', attrs={"id": "gp-quarter-1"}):
-    playObject = {
-        "timestamp": play.find('td', attrs={"class": "time-stamp"}).text.encode('utf-8'),
-        "gameDetails": play.find('td', attrs={"class": "game-details"}).text.encode('utf-8'),
-        "combinedScore": play.find('td', attrs={"class": "combined-score"}).text.encode('utf-8'),
-    }
-    playArray.append(playObject)
-with open('playData.json', 'w') as outfile:
-    json.dump(playArray, outfile)
+quarters = ["1", "2", "3", "4"] #trying to get the code to loop through all quarters of the game
+for i in range(len(quarters)):
+	for play in content.findAll('div', attrs={"id": "gp-quarter-" + str(i)}):
+	    playObject = {
+	        "timestamp": play.find('td', attrs={"class": "time-stamp"}).text.encode('utf-8'),
+	        "gameDetails": play.find('td', attrs={"class": "game-details"}).text.encode('utf-8'),
+	        "combinedScore": play.find('td', attrs={"class": "combined-score"}).text.encode('utf-8'),
+	    }
+	    playArray.append(playObject)
+	with open('playData.json', 'w') as outfile:
+	    json.dump(playArray, outfile)
+
     
