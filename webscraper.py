7880991ab87b035895579d9e7c086f973d8f03e7
wathiqabumaali <aeaabumaali@ucdavis.edu>
2019-05-01 14:06:59 -0700

    Got all players names
    Grabbed players names from box score. Next step is to get home team separated from away team. Then, separate starters from bench players

--- webscraper.py
+++ webscraper.py
@@ -2,15 +2,16 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-url = "http://www.espn.com/nba/playbyplay?gameId=401126819&wsVar=us~nba~gamepackage,desktop,en"
-response = requests.get(url, timeout=5)
-html = response.content
-soup = BeautifulSoup(html, "lxml")
+urlPlays = "http://www.espn.com/nba/playbyplay?gameId=401126819&wsVar=us~nba~gamepackage,desktop,en"
+responsePlays = requests.get(urlPlays, timeout=5)
+htmlPlays = responsePlays.content
+soupPlays = BeautifulSoup(htmlPlays, "lxml")
 
+# Lines 11-28 gets the play by play info
 playArray = []
 quarters = ["1", "2", "3", "4"] #trying to get the code to loop through all quarters of the game
 for quarter in quarters:
-    quarterBody = soup.find('div', attrs={"id": "gp-quarter-" + quarter})
+    quarterBody = soupPlays.find('div', attrs={"id": "gp-quarter-" + quarter})
     for row in quarterBody.findAll('tr'):
         playObject = {}
         for time in row.findAll('td', attrs={"class": "time-stamp"}):
@@ -25,3 +26,23 @@ for quarter in quarters:
             playArray.append(playObject)
         with open('playData.json', 'w') as outfile:
             json.dump(playArray, outfile)
+
+# Got all players names
+
+urlPlayers = "http://www.espn.com/nba/boxscore?gameId=401126819&wsVar=us~nba~gamepackage,desktop,en"
+responsePlayers = requests.get(urlPlayers, timeout=5)
+htmlPlayers = responsePlayers.content
+soupPlayers = BeautifulSoup(htmlPlayers, "lxml")
+
+playersArray = []
+for columnBody in soupPlayers.find('div', attrs={"id": "gamepackage-boxscore-module"}):
+    for eachRow in columnBody.findAll('tr'):
+        playersObject = {}
+        for player in eachRow.findAll('td', attrs={"class": "name"}):
+            for playerAbbrev in player.findAll('span', attrs={"class": "abbr"}):
+                text = playerAbbrev.text
+                playersObject = {"name": text}
+                playersArray.append(playersObject)
+            with open('playerData.json', 'w') as outfile:
+                json.dump(playersArray, outfile)
+
